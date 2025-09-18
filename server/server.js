bb052f64223bb20b88921c6e0474fd34108855ba
Tasmia Chowdhury <tasmiachowdhury123@gmail.com>
2025-09-17 20:55:15 -0400

    checkpoint 3

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import giftsRouter from './routes/gifts.js'
 
 const app = express()
 
@@ -8,6 +9,10 @@ app.use('/public', express.static('./public'))
 app.get('/', (req, res) => {
   res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
 })
+
+
+
+app.use('/gifts', giftsRouter)
 
 const PORT = process.env.PORT || 3001
     
