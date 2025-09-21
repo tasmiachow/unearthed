86de0e40c05f199050394df33e6b9f583c6d1087
Krish A. Patel <krishpatel2067@gmail.com>
2025-09-21 18:59:31 -0400

    Add editorconfig file

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use('/public', express.static('./public'))
 app.use('/scripts', express.static('./public/scripts'))
 
 app.get('/', (req, res) => {
-  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
 })
 
 app.use('/gifts', giftsRouter)
@@ -15,5 +15,5 @@ app.use('/gifts', giftsRouter)
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening on http://localhost:${PORT}`)
+    console.log(`🚀 Server listening on http://localhost:${PORT}`)
 })
