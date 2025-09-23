a08f71833d642cacf7ee7310f6999e350c23baf6
Ian-Acosta2330 <ian2330acosta@gmail.com>
2025-09-23 12:52:54 -0600

    reconfigured file structure, checkpoint 5

---
+++ client/scripts/header.js
@@ -0,0 +1,32 @@
+const header = document.querySelector('header')
+
+const headerContainer=document.createElement('div')
+headerContainer.className = 'header-container'
+
+const headerLeft = document.createElement('div')
+headerLeft.className='header-left'
+
+const headerImg = document.createElement('img')
+headerImg.src = 'logo.png'
+
+const headerH1 = document.createElement('h1')
+headerH1.textContent="UnEarthed"
+
+headerLeft.appendChild(headerImg)
+headerLeft.appendChild(headerH1)
+
+const headerRight = document.createElement('div')
+headerRight.className='header-right'
+
+const headerButton = document.createElement('Home')
+headerButton.textContent='Home'
+headerButton.addEventListener('click',function handleClick(event) {
+    window.location='/'
+})
+
+headerRight.appendChild(headerButton)
+
+headerContainer.appendChild(headerLeft)
+headerContainer.appendChild(headerRight)
+
+header.appendChild(headerContainer)
--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -13,7 +13,7 @@ router.get('/', (req,res) => {
 })
 
 router.get('/:giftId', (req,res) => {
-    res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
+    res.status(200).sendFile(path.resolve(__dirname, '../gift.html'))
 })
 
 export default router
